dfea4469084672b590bd731e448f54c91f4f96b3
Ignusmart <ignusmart@gmail.com>
2017-02-24 20:50:17 -0500

    Views implemented

--- myApp/urls.py
+++ myApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     # ex: /polls/
-    url(r'^$', views.index, name='index'),
+    url(r'^first$', views.first_container, name='first_container'),
+    url(r'^second$', views.second_container, name='second_container'),
 ]
--- myApp/views.py
+++ myApp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
-def index(request):
+def first_container(request):
     context = {'container':'FirstContainer', 'page_title':'First Container'}
+    return render(request, 'index.html', context=context)
+
+def second_container(request):
+    context = {'container':'SecondContainer', 'page_title':'Second Container'}
     return render(request, 'index.html', context=context)
