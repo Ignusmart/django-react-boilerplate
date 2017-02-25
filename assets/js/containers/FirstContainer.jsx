c5d666daf4cc767fa52cc585924115d0184ca66c
Ignusmart <ignusmart@gmail.com>
2017-02-24 20:44:54 -0500

    css modules initial dev

--- assets/js/containers/FirstContainer.jsx
+++ assets/js/containers/FirstContainer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+let styles = require('./FirstContainer.scss');
 
 class FirstContainer extends React.Component {
     render() {
       return (
-        <h1> Hello, From first container! </h1>
+        <h1 className={styles.title}> Hello, From first container! </h1>
       )
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,11 @@ var files = fs.readdirSync(path.resolve(ENTRY_DIR));
 var entries = {};
 
 files.forEach(fileName => {
-  var entryName = fileName.split('.')[0];
-  entries[entryName] = ENTRY_DIR + '/' + fileName;
+  var extRegExp = /\.jsx/;
+  if(extRegExp.test(fileName)){
+    var entryName = fileName.split('.')[0];
+    entries[entryName] = ENTRY_DIR + '/' + fileName;
+  }
 });
 
 module.exports = {
@@ -34,6 +37,22 @@ module.exports = {
           query: {
             presets: ['es2015', 'stage-0', 'react']
           }
+        },
+        {
+          test: /\.css$/,
+          loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' 
+        },
+        {
+          test: /\.(scss)$/,
+          use: [{
+            loader: 'style-loader'
+          }, {
+            loader: 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:6]'
+          }, {
+            loader: 'resolve-url-loader'
+          }, {
+            loader: 'sass-loader'
+          }]
         }
       ]
     },
